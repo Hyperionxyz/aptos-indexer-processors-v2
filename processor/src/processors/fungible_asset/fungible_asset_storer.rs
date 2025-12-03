6044bec8749cdf09359a9e242b6cd729d03a6f97
flex <flex@caas.xyz>
2025-12-03 20:16:15 +0800

    Remove unused fungible asset activities handling from processor

--- processor/src/processors/fungible_asset/fungible_asset_storer.rs
+++ processor/src/processors/fungible_asset/fungible_asset_storer.rs
@@ -85,7 +85,7 @@ impl Processable for FungibleAssetStorer {
         )>,
     ) -> Result<Option<TransactionContext<Self::Output>>, ProcessorError> {
         let (
-            fungible_asset_activities,
+            _fungible_asset_activities,
             fungible_asset_metadata,
             _,
             (current_unified_fab_v1, current_unified_fab_v2),
@@ -98,26 +98,15 @@ impl Processable for FungibleAssetStorer {
         let (
             current_unified_fab_v1,
             current_unified_fab_v2,
-            fungible_asset_activities,
             fungible_asset_metadata,
             fa_to_coin_mappings,
         ) = filter_datasets!(self, {
             current_unified_fab_v1 => TableFlags::CURRENT_FUNGIBLE_ASSET_BALANCES,
             current_unified_fab_v2 => TableFlags::CURRENT_FUNGIBLE_ASSET_BALANCES,
-            fungible_asset_activities => TableFlags::FUNGIBLE_ASSET_ACTIVITIES,
             fungible_asset_metadata => TableFlags::FUNGIBLE_ASSET_METADATA,
             fa_to_coin_mappings => TableFlags::FUNGIBLE_ASSET_TO_COIN_MAPPINGS,
         });
 
-        let faa = execute_in_chunks(
-            self.conn_pool.clone(),
-            insert_fungible_asset_activities_query,
-            &fungible_asset_activities,
-            get_config_table_chunk_size::<PostgresFungibleAssetActivity>(
-                "fungible_asset_activities",
-                &per_table_chunk_sizes,
-            ),
-        );
         let fam = execute_in_chunks(
             self.conn_pool.clone(),
             insert_fungible_asset_metadata_query,
@@ -154,9 +143,9 @@ impl Processable for FungibleAssetStorer {
                 &per_table_chunk_sizes,
             ),
         );
-        let (faa_res, fam_res, cufab1_res, cufab2_res, fatcm_res) =
-            tokio::join!(faa, fam, cufab_v1, cufab_v2, fatcm);
-        for res in [faa_res, fam_res, cufab1_res, cufab2_res, fatcm_res] {
+        let (fam_res, cufab1_res, cufab2_res, fatcm_res) =
+            tokio::join!(fam, cufab_v1, cufab_v2, fatcm);
+        for res in [fam_res, cufab1_res, cufab2_res, fatcm_res] {
             match res {
                 Ok(_) => {},
                 Err(e) => {
@@ -184,18 +173,6 @@ impl NamedStep for FungibleAssetStorer {
     fn name(&self) -> String {
         "FungibleAssetStorer".to_string()
     }
-}
-
-pub fn insert_fungible_asset_activities_query(
-    items_to_insert: Vec<PostgresFungibleAssetActivity>,
-) -> impl QueryFragment<Pg> + diesel::query_builder::QueryId + Send {
-    use schema::fungible_asset_activities::dsl::*;
-
-    diesel::insert_into(schema::fungible_asset_activities::table)
-        .values(items_to_insert)
-        .on_conflict((transaction_version, event_index))
-        .do_update()
-        .set(storage_id.eq(excluded(storage_id)))
 }
 
 pub fn insert_fungible_asset_metadata_query(
